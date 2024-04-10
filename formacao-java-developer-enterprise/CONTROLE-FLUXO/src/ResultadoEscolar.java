b725f7a6c8c1f18e7ae80775690fc630f55eec1b
Elys Sanntos <elys.santos@hotmail.com>
2024-04-10 08:27:51 -0300

    Condicional Encadeada

--- formacao-java-developer-enterprise/CONTROLE-FLUXO/src/ResultadoEscolar.java
+++ formacao-java-developer-enterprise/CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -2,10 +2,13 @@ public class ResultadoEscolar {
 
     public static void main(String[] args) {
 
-        int nota = 8;
+        int nota = 10;
  
-        if(nota >= 7)
+        if(nota >= 7) // Sempre retorna true
          System.out.println("Aprovado");
+
+        else if(nota >=5 && nota <7) // Retorna true or false
+        System.out.println("Prova Recuperação");
  
         else
          System.out.println("Reprovado");
