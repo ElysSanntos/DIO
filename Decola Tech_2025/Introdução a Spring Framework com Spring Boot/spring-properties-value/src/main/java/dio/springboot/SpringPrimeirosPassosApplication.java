91af2fd38e0a8efd41c791b04ebbe837b3116df2
Elys Sanntos <elys.santos@hotmail.com>
2025-02-15 16:25:51 -0300

    Organizando arquivos de estudos

--- Decola Tech_2025/Introdução a Spring Framework com Spring Boot/spring-properties-value/src/main/java/dio/springboot/SpringPrimeirosPassosApplication.java
+++ Decola Tech_2025/Introdução a Spring Framework com Spring Boot/spring-properties-value/src/main/java/dio/springboot/SpringPrimeirosPassosApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPrimeirosPassosApplication {
+public class  SpringPrimeirosPassosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPrimeirosPassosApplication.class, args);
